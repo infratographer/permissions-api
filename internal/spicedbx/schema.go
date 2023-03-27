f6fca061a8a41a6436b18af2bd523223db37f4fb
John Schaeffer <jschaeffer@equinix.com>
2023-03-27 17:11:31 +0000

    Add support for schema generation
    The SpiceDB schema for permissions-api was previously hardcoded in
    text. This commit adds a template for the SpiceDB schema as well as
    data types for defining resources and actions. An instance of the
    Schema struct is currently hardcoded in the "schema" command to match
    existing output.
    
    Signed-off-by: John Schaeffer <jschaeffer@equinix.com>

--- internal/spicedbx/schema.go
+++ internal/spicedbx/schema.go
@@ -1,39 +1,86 @@
 package spicedbx
 
 import (
-	"strings"
+	"bytes"
+	"text/template"
+
+	"go.infratographer.com/permissions-api/internal/types"
 )
 
-func GeneratedSchema(prefix string) string {
-	schema := `
-definition PREFIX/subject {}
+var (
+	schemaTemplate = template.Must(template.New("schema").Parse(`
+{{$prefix := .Prefix}}
+definition {{$prefix}}/subject {}
 
-definition PREFIX/role {
-    relation tenant: PREFIX/tenant
-    relation subject: PREFIX/subject
+definition {{$prefix}}/role {
+    relation tenant: {{$prefix}}/tenant
+    relation subject: {{$prefix}}/subject
 }
 
-definition PREFIX/tenant {
-    relation parent: PREFIX/tenant
-
-    relation loadbalancer_create_rel: PREFIX/role#subject
-    relation loadbalancer_list_rel: PREFIX/role#subject
-    relation loadbalancer_get_rel: PREFIX/role#subject
-    relation loadbalancer_update_rel: PREFIX/role#subject
-    relation loadbalancer_delete_rel: PREFIX/role#subject
-
-    permission loadbalancer_create = loadbalancer_create_rel + parent->loadbalancer_create
-    permission loadbalancer_list = loadbalancer_list_rel + parent->loadbalancer_list
-    permission loadbalancer_get = loadbalancer_get_rel + parent->loadbalancer_get
-    permission loadbalancer_update = loadbalancer_update_rel + parent->loadbalancer_update
-    permission loadbalancer_delete = loadbalancer_delete_rel + parent->loadbalancer_delete
+definition {{$prefix}}/tenant {
+    relation tenant: {{$prefix}}/tenant
+{{- range .ResourceTypes -}}
+{{$typeName := .Name}}
+{{range .TenantActions}}
+    relation {{$typeName}}_{{.}}_rel: {{$prefix}}/role#subject
+{{- end}}
+{{range .TenantActions}}
+    permission {{$typeName}}_{{.}} = {{$typeName}}_{{.}}_rel + tenant->{{$typeName}}_{{.}}
+{{- end}}
+{{- end}}
 }
-`
+{{range .ResourceTypes -}}
+{{$typeName := .Name}}
+definition {{$prefix}}/{{$typeName}} {
+    relation tenant: {{$prefix}}/tenant
+{{range .TenantActions}}
+    relation {{$typeName}}_{{.}}_rel: {{$prefix}}/role#subject
+{{- end}}
+{{range .TenantActions}}
+    permission {{$typeName}}_{{.}} = {{$typeName}}_{{.}}_rel + tenant->{{$typeName}}_{{.}}
+{{- end}}
+}
+{{end}}
+`))
+)
 
-	if prefix != "" && !strings.HasSuffix(prefix, "/") {
-		prefix += "/"
+func GenerateSchema(namespace string, resourceTypes []types.ResourceType) (string, error) {
+	var data struct {
+		Prefix        string
+		ResourceTypes []types.ResourceType
 	}
 
-	return strings.ReplaceAll(schema, "PREFIX/", prefix)
+	data.Prefix = namespace
+	data.ResourceTypes = resourceTypes
 
+	var out bytes.Buffer
+
+	err := schemaTemplate.Execute(&out, data)
+	if err != nil {
+		return "", err
+	}
+
+	return out.String(), nil
+}
+
+func GeneratedSchema(prefix string) string {
+	resourceTypes := []types.ResourceType{
+		{
+			Name: "loadbalancer",
+			TenantActions: []string{
+				"create",
+				"get",
+				"list",
+				"update",
+				"delete",
+			},
+		},
+	}
+
+	schema, err := GenerateSchema(prefix, resourceTypes)
+	if err != nil {
+		panic(err)
+	}
+
+	return schema
 }
